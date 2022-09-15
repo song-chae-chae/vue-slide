4ac225e358ec65bc704fe8b17a737fd784096470
cy28 <cy28@springsoft.io>
2022-09-15 17:14:50 +0900

    add scss style

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: false,
+  arrowParens: "always",
+  printWidth: 120,
+  endOfLine: "auto",
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        // options here will be passed to css-loader
+        additionalData: `@import "@/assets/styles/vs-common.scss";`,
+      },
+    },
+  },
 });
