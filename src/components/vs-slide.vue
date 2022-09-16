685a568c9297a86c79644e695a9a3a67965ac11a
cy28 <cy28@springsoft.io>
2022-09-16 20:08:19 +0900

    add default slide component

---
+++ src/components/vs-slide.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="slide-wrapper">
+    <splide :options="options">
+      <splide-slide>
+        <img src="https://cdn.pixabay.com/photo/2017/07/28/14/23/macarons-2548810__340.jpg" alt="image1" />
+      </splide-slide>
+      <splide-slide>
+        <img src="https://cdn.pixabay.com/photo/2018/03/11/09/08/cookie-3216243__340.jpg" alt="image2" />
+      </splide-slide>
+    </splide>
+  </div>
+</template>
+
+<script>
+import {Splide, SplideSlide} from "@splidejs/vue-splide";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+export default {
+  components: {
+    Splide,
+    SplideSlide,
+  },
+  data() {
+    return {
+      options: {
+        rewind: true,
+        gap: "1rem",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
