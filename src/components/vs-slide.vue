06300c7298256d5e4a16c93b76bb26075011eebc
chaechae <songchaechaes@gmail.com>
2022-09-18 23:23:48 +0900

    use v-for to control slides dynamically

--- src/components/vs-slide.vue
+++ src/components/vs-slide.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="slide-wrapper">
     <splide :options="options" @splide:pagination:updated="onPaginationUpdated" @splide:move="onMoved">
-      <splide-slide>
-        <img src="https://cdn.pixabay.com/photo/2017/07/28/14/23/macarons-2548810__340.jpg" alt="image1" />
-      </splide-slide>
-      <splide-slide>
-        <img src="https://cdn.pixabay.com/photo/2018/03/11/09/08/cookie-3216243__340.jpg" alt="image2" />
-      </splide-slide>
-      <splide-slide>
-        <img src="https://cdn.pixabay.com/photo/2015/07/01/08/41/afternoon-tea-827395__340.jpg" alt="image3" />
-      </splide-slide>
-      <splide-slide>
-        <img src="https://cdn.pixabay.com/photo/2016/03/27/19/23/tart-1283822__340.jpg" alt="image4" />
+      <splide-slide v-for="(value, index) in images" :key="index">
+        <img :src="value" :alt="`image${index + 1}`" :class="[index === 0 ? 'next' : 'previous']" />
       </splide-slide>
     </splide>
   </div>
@@ -33,6 +24,12 @@ export default {
         type: "loop",
         padding: "5rem",
       },
+      images: [
+        "https://cdn.pixabay.com/photo/2017/07/28/14/23/macarons-2548810__340.jpg",
+        "https://cdn.pixabay.com/photo/2018/03/11/09/08/cookie-3216243__340.jpg",
+        "https://cdn.pixabay.com/photo/2015/07/01/08/41/afternoon-tea-827395__340.jpg",
+        "https://cdn.pixabay.com/photo/2016/03/27/19/23/tart-1283822__340.jpg",
+      ],
     };
   },
   methods: {
